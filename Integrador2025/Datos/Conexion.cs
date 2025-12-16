7e27c28888e6b09a187894a8fa07aed1c5606cdb
Rodrigo Nicolás Romero <123268630+NicolasRomero2165@users.noreply.github.com>
2025-12-15 21:29:05 -0300

    Add Conexion class for database connection

---
+++ Integrador2025/Datos/Conexion.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrador2025.Datos
+{
+    public class Conexion
+    {
+        public static string sConnection = @"server = PATATA\SQLEXPRESS; database = INTEGRADORNET; Integrated Security=True";
+       
+    }
+}
